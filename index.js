f6152d8d7cb603696db1691f8adbe6da9b013d98
Cyra Dawn <cyradawn@gmail.com>
2017-11-28 19:13:17 +0800

    push

---
+++ index.js
@@ -0,0 +1,35 @@
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+// app.get('/', function(request , response){
+// 	response.sendFile(__dirname + '/public/BoxPong.html');
+// });
+
+
+// io.on('connection', function(socket){
+// 	console.log('a user connected');
+// 	socket.on('disconnect', function(){
+// 		console.log('user disconnected');
+// 	});
+// });
+
+// http.listen(3000, function(){
+// 	console.log('listening on port: 3000');
+
+//  });
+
+var express = require('express')
+var app = express();
+var server = require('http').createServer(app);
+
+app.use(express.static(__dirname + '/public')); 
+//redirect / to our index.html file
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/public/BoxPong.html');
+});
+	
+//start our web server and socket.io server listening
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
